b78bd5ee337411b1566fc0328108a2081c6fa235
Alexey Zhumaev <alexey221284@mail.ru>
2020-03-22 22:25:14 +0400

    feat: add function in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  str=str.split("");
+  let open=[];
+  let close=[];
+  let sam=[];
+  
+  for (let i=0; i<bracketsConfig.length; i++) {
+    if (bracketsConfig[i][0]==bracketsConfig[i][1]) {
+      sam.push(bracketsConfig[i][0]);
+    } else {
+      open.push(bracketsConfig[i][0]);
+      close.push(bracketsConfig[i][1]);
+    }
+  }
+
+  let stack=[];
+  for (i=0; i<str.length; i++) {
+    if (open.includes(str[i])) {
+      stack.push(str[i]);
+    } else if (close.includes(str[i])) {
+      if (open.indexOf(stack[stack.length-1])==close.indexOf(str[i])) {
+        stack.pop();
+      } else return false;
+    } else if (stack[stack.length-1]==str[i]) {
+      stack.pop();
+    } else {
+      stack.push(str[i]);
+    }
+  }
+    if (stack.length==0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
